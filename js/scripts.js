c38e17c0a2ff8cff859a7c93573c961430d00621
Abel Trotter <a_trotter0@hotmail.com>
2018-05-29 19:02:39 -0700

    Add card clip class and adjust styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   $(".card").click(function() {
-    $(this).children("p").fadeToggle();
+    $(this).children("h3").toggle();
+    $(this).children("p").fadeToggle(1200);
+    $(this).toggleClass("card-flip");
   });
 });
